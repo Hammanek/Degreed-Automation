8947f8244a3588c7cb4817ce6b9096a8e8d46f10
Hammanek <hammanecek@gmail.com>
2026-03-11 00:16:32 +0100

    Přidán setlocal a úprava syntaxe pro potlačení výpisu příkazů

--- START.bat
+++ START.bat
@@ -1,4 +1,5 @@
 ﻿@echo off
+setlocal enabledelayedexpansion
 title Degreed Automatizace
 
 cls
@@ -30,20 +31,28 @@ echo Kontrola prohlizece...
 echo.
 
 REM Kontrola/Stazeni prenosneho Chromia
-set CHROMIUM_DIR=%~dp0chromium
-set CHROMIUM_EXE=
+set "CHROMIUM_DIR=%~dp0chromium"
+set "CHROMIUM_EXE="
 
-if exist "%CHROMIUM_DIR%\chrome-win\chrome.exe" set CHROMIUM_EXE=%CHROMIUM_DIR%\chrome-win\chrome.exe >nul 2>&1
-if exist "%CHROMIUM_DIR%\chrome-win32\chrome.exe" set CHROMIUM_EXE=%CHROMIUM_DIR%\chrome-win32\chrome.exe >nul 2>&1
+if exist "%CHROMIUM_DIR%\chrome-win\chrome.exe" (
+    set "CHROMIUM_EXE=%CHROMIUM_DIR%\chrome-win\chrome.exe"
+)
+if exist "%CHROMIUM_DIR%\chrome-win32\chrome.exe" (
+    set "CHROMIUM_EXE=%CHROMIUM_DIR%\chrome-win32\chrome.exe"
+)
 
 if defined CHROMIUM_EXE goto :browser_ready
 
 echo Prenosny prohlizec nenalezen. Stahuji Chromium...
-if not exist "%CHROMIUM_DIR%" mkdir "%CHROMIUM_DIR%" >nul 2>&1
+if not exist "%CHROMIUM_DIR%" mkdir "%CHROMIUM_DIR%"
 powershell -Command "& {Write-Host 'Stahuji...'; $url = 'https://github.com/RobRich999/Chromium_Clang/releases/latest/download/chrome.zip'; Invoke-WebRequest -Uri $url -OutFile '%CHROMIUM_DIR%\chromium.zip'; Write-Host 'Rozbaluji...'; Expand-Archive -Path '%CHROMIUM_DIR%\chromium.zip' -DestinationPath '%CHROMIUM_DIR%' -Force; Remove-Item '%CHROMIUM_DIR%\chromium.zip'}"
 
-if exist "%CHROMIUM_DIR%\chrome-win\chrome.exe" set CHROMIUM_EXE=%CHROMIUM_DIR%\chrome-win\chrome.exe >nul 2>&1
-if exist "%CHROMIUM_DIR%\chrome-win32\chrome.exe" set CHROMIUM_EXE=%CHROMIUM_DIR%\chrome-win32\chrome.exe >nul 2>&1
+if exist "%CHROMIUM_DIR%\chrome-win\chrome.exe" (
+    set "CHROMIUM_EXE=%CHROMIUM_DIR%\chrome-win\chrome.exe"
+)
+if exist "%CHROMIUM_DIR%\chrome-win32\chrome.exe" (
+    set "CHROMIUM_EXE=%CHROMIUM_DIR%\chrome-win32\chrome.exe"
+)
 
 if not defined CHROMIUM_EXE (
     echo Nepodarilo se pripravit Chromium.
@@ -52,14 +61,14 @@ if not defined CHROMIUM_EXE (
 )
 
 :browser_ready
-if not exist "%~dp0browser_profile" mkdir "%~dp0browser_profile" >nul 2>&1
-set BROWSER_FLAGS=--load-extension="%~dp0extension" --user-data-dir="%~dp0browser_profile" --no-first-run --no-default-browser-check --disable-popup-blocking --disable-background-timer-throttling --disable-renderer-backgrounding --disable-backgrounding-occluded-windows
+if not exist "%~dp0browser_profile" mkdir "%~dp0browser_profile"
+set "BROWSER_FLAGS=--load-extension="%~dp0extension" --user-data-dir="%~dp0browser_profile" --no-first-run --no-default-browser-check --disable-popup-blocking --disable-background-timer-throttling --disable-renderer-backgrounding --disable-backgrounding-occluded-windows"
 
 echo Spoustim...
 
 REM Kontrola, jestli je to první spuštění (prázdný browser_profile)
-set FIRST_RUN=0
-if not exist "%~dp0browser_profile\Default" set FIRST_RUN=1 >nul 2>&1
+set "FIRST_RUN=0"
+if not exist "%~dp0browser_profile\Default" set "FIRST_RUN=1"
 
 if %FIRST_RUN%==1 (
     echo.
