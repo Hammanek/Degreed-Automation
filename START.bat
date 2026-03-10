1a5cecb2eb8116e3778399b00347d5af555d8f80
Hammanek <hammanecek@gmail.com>
2026-03-11 00:18:48 +0100

    Dokončení potlačení výpisu - přidán @ před všechny echo příkazy

--- START.bat
+++ START.bat
@@ -2,20 +2,20 @@
 setlocal enabledelayedexpansion
 title Degreed Automatizace
 
-cls
-echo ================================================================
-echo            DEGREED AUTOMATIZACE - PORTABLE
-echo ================================================================
-echo.
+@cls
+@echo ================================================================
+@echo            DEGREED AUTOMATIZACE - PORTABLE
+@echo ================================================================
+@echo.
 
 REM Kontrola existuje url.txt
-if exist "url.txt" goto :links_ok
-echo Nelezen soubor url.txt!
-echo.
-echo Vytvor soubor url.txt s odkazy na kurzy.
-echo.
-pause
-exit /b
+@if exist "url.txt" goto :links_ok
+@echo Nelezen soubor url.txt!
+@echo.
+@echo Vytvor soubor url.txt s odkazy na kurzy.
+@echo.
+@pause
+@exit /b
 
 :links_ok
 @echo off
@@ -27,9 +27,9 @@ REM Kontrola odkazu
     exit /b
 )
 
-echo.
-echo Kontrola prohlizece...
-echo.
+@echo.
+@echo Kontrola prohlizece...
+@echo.
 
 REM Kontrola/Stazeni prenosneho Chromia
 @set "CHROMIUM_DIR=%~dp0chromium"
@@ -44,7 +44,7 @@ REM Kontrola/Stazeni prenosneho Chromia
 
 @if defined CHROMIUM_EXE goto :browser_ready
 
-echo Prenosny prohlizec nenalezen. Stahuji Chromium...
+@echo Prenosny prohlizec nenalezen. Stahuji Chromium...
 @if not exist "%CHROMIUM_DIR%" mkdir "%CHROMIUM_DIR%"
 @powershell -Command "& {Write-Host 'Stahuji...'; $url = 'https://github.com/RobRich999/Chromium_Clang/releases/latest/download/chrome.zip'; Invoke-WebRequest -Uri $url -OutFile '%CHROMIUM_DIR%\chromium.zip'; Write-Host 'Rozbaluji...'; Expand-Archive -Path '%CHROMIUM_DIR%\chromium.zip' -DestinationPath '%CHROMIUM_DIR%' -Force; Remove-Item '%CHROMIUM_DIR%\chromium.zip'}"
 
@@ -66,7 +66,7 @@ echo Prenosny prohlizec nenalezen. Stahuji Chromium...
 @if not exist "%~dp0browser_profile" mkdir "%~dp0browser_profile"
 @set "BROWSER_FLAGS=--load-extension="%~dp0extension" --user-data-dir="%~dp0browser_profile" --no-first-run --no-default-browser-check --disable-popup-blocking --disable-background-timer-throttling --disable-renderer-backgrounding --disable-backgrounding-occluded-windows"
 
-echo Spoustim...
+@echo Spoustim...
 
 REM Kontrola, jestli je to první spuštění (prázdný browser_profile)
 @set "FIRST_RUN=0"
